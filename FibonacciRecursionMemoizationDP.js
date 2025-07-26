6bdf583e99ddf2cf6ba39ed8f3e15e99f7c282a6
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2025-07-26 12:32:48 -0400

    Create FibonacciRecursionMemoizationDP.js

---
+++ FibonacciRecursionMemoizationDP.js
@@ -0,0 +1,8 @@
+function fib(n, memo = {}) {
+  if (n <= 1) return n;
+  if (memo[n]) return memo[n];
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+console.log(fib(10)); // 55
